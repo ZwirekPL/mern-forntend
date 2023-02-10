682729ab556d2eb1809eb2dac2dc5ed1305e3f2f
Zwirek_Dev <glanzgarage@gmail.com>
2023-02-10 10:56:02 +0100

    add boostrap. title of website.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +13,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button onClick={() => setClickCount(clickCount + 1)}>
+          Clicked {clickCount} times
+        </Button>
       </header>
     </div>
   );
